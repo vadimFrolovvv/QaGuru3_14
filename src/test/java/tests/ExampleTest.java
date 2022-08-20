670c8c9cb13872298a7571217347e5d7ff836515
Vadim <f.vadim.frolov@gmail.com>
2022-08-20 11:32:27 +0300

    first commit

---
+++ src/test/java/tests/ExampleTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void exampleTest() {
+        Assertions.assertTrue( 3 > 2);
+    }
+}
